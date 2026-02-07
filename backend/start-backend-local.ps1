e82e38d933c92ff5fa70dba19ccc4974370ae38f
llCelmarll <quentin.ddc@hotmail.fr>
2026-02-07 16:58:29 +0100

    Workflow dev local

--- backend/start-backend-local.ps1
+++ backend/start-backend-local.ps1
@@ -1,2 +1,10 @@
 ..\.venv\Scripts\activate
+
+# Charger les variables depuis .env
+Get-Content .env | ForEach-Object {
+    if ($_ -match '^\s*([^#][^=]+)=(.*)$') {
+        [Environment]::SetEnvironmentVariable($matches[1].Trim(), $matches[2].Trim(), "Process")
+    }
+}
+
 python -m uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
